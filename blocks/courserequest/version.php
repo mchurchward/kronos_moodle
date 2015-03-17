53d549f7891a412b5202930eec38c6dd3e79d6b8
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-03-17 11:02:47 -0400

    ELIS-9083: Update version releases & build dates.

--- blocks/courserequest/version.php
+++ blocks/courserequest/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014082502;
-$plugin->release = '2.7.3.0 (Build: 20141110)';
+$plugin->release = '2.7.7.0 (Build: 20150317)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'local_elisprogram' => 2014082503
--- local/elisreports/version.php
+++ local/elisreports/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2014082503;
-$plugin->release = '2.7.5.0 (Build: 20150202)';
+$plugin->release = '2.7.7.0 (Build: 20150317)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'local_eliscore' => 2014082503
