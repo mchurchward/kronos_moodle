09e5c47d0437431cf5ccfb6a11545cfb6e52943a
Akinsaya Delamarre <adelamarre@remote=-learner.net>
2015-12-03 12:41:36 -0500

    KRONOSDEV-119: Bumping version to .03 to bring it inline with the client's CCRMS.

--- local/elisprogram/version.php
+++ local/elisprogram/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014082507.02;
+$plugin->version = 2014082507.03;
 $plugin->release = '2.7.10.1 (Build: 20151021)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
